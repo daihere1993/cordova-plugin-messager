58187196eaeef968b7731e08b3c8e6cf1c4cc61d
daihere <daihere1993@gmail.com>
2017-07-07 18:02:58 +0800

    Fix bug

--- www/cordova-plugin-messager.js
+++ www/cordova-plugin-messager.js
@@ -3,10 +3,10 @@ var exec = require('cordova/exec');
 module.exports = {
   // receive message
   subscribe: function (param, successFn, failureFn) {
-    exec(successFn, failureFn, 'subscribe', [param]);
+    exec(successFn, failureFn, 'Messager', 'subscribe', [param]);
   },
   // publish message
   publish: function (param, successFn, failureFn) {
-    exec(successFn, failureFn, 'publish', [param]);
+    exec(successFn, failureFn, 'Messager', 'publish', [param]);
   }
 }
