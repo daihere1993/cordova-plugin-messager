c1ac49781b1c8af27907f0db977b93b8f5650f6c
daihere <daihere1993@gmail.com>
2017-07-12 17:41:48 +0800

    Modify android code

--- src/android/Messager.java
+++ src/android/Messager.java
@@ -5,12 +5,8 @@ import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.apache.cordova.CallbackContext;
 
-import android.os.Message;
-
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
-import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
-import org.eclipse.paho.client.mqttv3.util.Strings;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -20,8 +16,13 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Messager extends CordovaPlugin {
+    protected MqttClient client;
+
     protected void subscribe(CordovaArgs args, final CallbackContext callbackContext) {
         String hostHeader = "tcp://";
         String topic = "";
@@ -29,6 +30,7 @@ public class Messager extends CordovaPlugin {
         String host = "";
         String username = "";
         String password = "";
+        final Map<String, Object> callbackContextMap = new HashMap<String, Object>();
         final JSONObject params;
         try {
             params = args.getJSONObject(0);
@@ -37,36 +39,47 @@ public class Messager extends CordovaPlugin {
             host = hostHeader.concat(params.getString("host"));
             username = params.getString("username");
             password = params.getString("password");
+            callbackContextMap.put(topic, callbackContext);
         } catch (JSONException e) {
             callbackContext.error("参数格式错误");
         }
 
         try {
-            MqttClient client = new MqttClient(host, clientId, new MemoryPersistence());
-            MqttConnectOptions options = new MqttConnectOptions();
-            options.setUserName(username);
-            options.setPassword(password.toCharArray());
-            options.setCleanSession(true);
-            options.setConnectionTimeout(10);
-            options.setKeepAliveInterval(20);
-            client.setCallback(new MqttCallback() {
-                @Override
-                public void connectionLost(Throwable cause) {
-                    callbackContext.error("链接失败");
-                }
+            if (client == null) {
+                client = new MqttClient(host, clientId, new MemoryPersistence());
+                MqttConnectOptions options = new MqttConnectOptions();
+                options.setUserName(username);
+                options.setPassword(password.toCharArray());
+                options.setCleanSession(true);
+                options.setConnectionTimeout(10);
+                options.setKeepAliveInterval(20);
+                client.connect(options);
+            } else if (client.isConnected()) {
+                client.setCallback(new MqttCallback() {
+                    @Override
+                    public void connectionLost(Throwable cause) {
+                        callbackContext.error("连接失败");
+                    }
 
-                @Override
-                public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
-                    callbackContext.success(new String(mqttMessage.getPayload(), "UTF-8"));
-                }
+                    @Override
+                    public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
+                        CallbackContext callbackContext2 = (CallbackContext)callbackContextMap.get(s);
+                        String msg = new String(mqttMessage.getPayload(), "UTF-8");
+                        PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, msg);
+                        pluginResult.setKeepCallback(true);
 
-                @Override
-                public void deliveryComplete(IMqttDeliveryToken token) {
-                    System.out.println("deliveryComplete-------" + token.isComplete());
-                }
-            });
-            client.connect(options);
-            client.subscribe(topic, 1);
+                        callbackContext2.sendPluginResult(pluginResult);
+                    }
+
+                    @Override
+                    public void deliveryComplete(IMqttDeliveryToken token) {
+                        System.out.println("deliveryComplete-------" + token.isComplete());
+                    }
+                });
+                client.subscribe(topic, 1);
+            } else {
+                callbackContext.error("未连接上消息服务器");
+            }
         } catch (MqttException e) {
             e.printStackTrace();
         }
