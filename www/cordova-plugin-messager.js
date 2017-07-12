9b373924f617f24f45927cc063516e194dd431b9
daihere <daihere1993@gmail.com>
2017-07-12 09:47:33 +0800

    Fix bug

--- www/cordova-plugin-messager.js
+++ www/cordova-plugin-messager.js
@@ -8,15 +8,9 @@ module.exports = {
   // receive message
   subscribe: function (param, successFn, failureFn) {
     exec(successFn, failureFn, 'Messager', 'subscribe', [param]);
-    return function (successCallback, failCallback) {
-      exec(successCallback, failCallback, 'Messager', 'disconnect', []);
-    };
   },
   // publish message
   publish: function (param, successFn, failureFn) {
     exec(successFn, failureFn, 'Messager', 'publish', [param]);
-    return function (successCallback, failCallback) {
-      exec(successCallback, failCallback, 'Messager', 'disconnect', []);
-    };
   }
 }
