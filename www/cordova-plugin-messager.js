8eb26fcac7244217d04393973dd4a732b2629c07
daihere <daihere1993@gmail.com>
2017-07-13 09:56:04 +0800

    Modify config

--- www/cordova-plugin-messager.js
+++ www/cordova-plugin-messager.js
@@ -10,3 +10,16 @@ module.exports = {
     exec(successFn, failureFn, 'Messager', 'publish', [param]);
   }
 }
+
+var params = {
+  clientId: 'test', // just a unique string
+  host: 'test.mosquitto.org:8883',
+  username: 'admin',
+  password: 'password',
+  topic: 'login'
+}
+cordova.plugins.messager.subscribe(params, function (msg) {
+  alert(msg);
+}, function (error)  {
+  console.log(error);
+});
